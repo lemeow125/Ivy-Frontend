5b93ff241ce6accb257671eb47649c3afe804d21
keannu125 <noehbernasol0@gmail.com>
2023-03-08 23:39:41 +0800

    Polished product page

--- src/Routes/Product/Product.tsx
+++ src/Routes/Product/Product.tsx
@@ -25,6 +25,7 @@ export default function Product() {
     mutationFn: DeleteProduct,
     onSuccess: () => {
       queryClient.invalidateQueries("products");
+      queryClient.invalidateQueries("product");
       queryClient.invalidateQueries("logs");
     },
   });
